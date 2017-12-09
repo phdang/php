0cd47e9874a7fd097a6d72b59a788034d599ed05
phdang <phdang94@yahoo..com>
2017-12-10 04:57:46 +0700

    some bugs

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Untitled Document</title>
+<title>Connect to MySQL</title>
 <style>
 	table,td,th {
 		border:1px solid grey;
@@ -13,6 +13,9 @@
 	}
 	th {
 		background-color:Turquoise;
+	}
+	select {
+		margin: 5px 0;
 	}
 	.le {
 		background-color:lavender;
@@ -27,56 +30,96 @@
 <?php
 
 	//first check
-	
+
 	//phpinfo();
-	
+
 	//dev mode
-	
+
 	error_reporting(E_ALL); //show errors
-	
+
 	//config
-	
+
 	$host = 'localhost';
 	$user = 'shop';
 	$pass = 'phd_shop 123';
 	$db = 'shop';
-	
+
 	//connect to MySQl
-	
+
 	$link = mysqli_connect($host,$user,$pass,$db) or die("Ket noi that bai");
-	
+
 	//UTF8
-	
+
 	mysqli_set_charset($link,'utf8');
-	
+
 	//cau truy van sql
-	
-	$sql = 'SELECT * FROM `phd4shop_department`';
-	
+
+	$sql = 'SELECT * FROM `nn_department` LIMIT 0,10';
+
 	// Query
-	
+
 	$rs = mysqli_query($link,$sql);
-	
+
 	// fetch 1 row
-	
-	//$r = mysqli_fetch_assoc($rs);
-	
+
+	//$r = mysqli_fetch_row($rs);
+	?>
+	<table>
+		<tr>
+			<th>ID</th>
+			<!-- <th>Dep ID</th> -->
+			<th>Name</th>
+			<th>Order</th>
+			<th>Active</th>
+		</tr>
+	<?php
+			// khai bao bien $len -- Declare variable $len
+			$len = null;
+			$j = 1;
+			while ($r = mysqli_fetch_row($rs)) { ?>
+			<tr class="<?= $j%2 ? 'le' : 'chan' ;?>">
+			<?php
+				//Khi $len khong co gia tri = null thi $len = count($r)
+				$len = $len ? $len : count($r);
+				for ($i=0; $i < $len; $i++) { ?>
+				<!-- 1 la vi tri cot khong muon canh giua, vi tri bat dau dem tu 0 -->
+				<td <?= $i != 1 ? 'align="center"' : '' ;?>><?= $r[$i]?></td>
+			<?php }
+				$j++;
+			 ?>
+			</tr>
+	<?php } ?>
+
+	</table>
+	<?php
 	// Display 1 row in column_name - Hien thi
-	
-	//echo $r['name'];
-	
+
+	//echo $r['name']
+
 	//Show all rows
-	
-	//while($r = mysqli_fetch_assoc($rs)) {	
+
+	//while($r = mysqli_fetch_assoc($rs)) {
 	//	echo $r['name'];
 	//}
 	?>
+<h2>Danh sach chung loai</h2>
+	<select>
+	<?php
+$sql = 'SELECT * FROM `nn_department` LIMIT 0,10';
+$rs = mysqli_query($link,$sql);
+while($r=mysqli_fetch_assoc($rs)){
+?>
+			<option value="<?= $r['id']?>"><?= $r['name']?></option>
+
+		<?php
+}
+		?>
+	</select>
    	<?php
-		$sql = 'SELECT * FROM `nn_category` LIMIT 0,10';
+		$sql = 'SELECT * FROM `nn_category`LIMIT 0,10';
 		$rs = mysqli_query($link,$sql);
 	?>
 	<table>
-    		<caption><h2>Danh sach chung loai</h2></caption>
     		<tr>
         		<th>STT</th>
         		<th>Dep ID</th>
@@ -84,12 +127,10 @@
             	<th>Thu Tu</th>
             	<th> An/Hien </th>
         	</tr>
-    	
 	<?php
 		$i = 1;
       		while($r = mysqli_fetch_assoc($rs)) {
-	?>	
-		 
+	?>
 		<tr class="<?= ($i%2) ? 'le' : 'chan'?>">
       	 	<td align="center"><?= $r['id'] ?><br></td>
 			<td align="center"><?= $r['department_id']?><br></td>
@@ -102,22 +143,5 @@
         }
 	?>
 	</table>
-    
-    	<select>
-    	<?php
-		$sql = 'SELECT * FROM `nn_department`';
-		$rs = mysqli_query($link,$sql);
-		while($r=mysqli_fetch_assoc($rs)){
-			
-		?>
-    
-    			<option value="<?= $r['id']?>"><?= $r['name']?></option>
-        
-        <?php
-		}
-        ?>
-        
-    	</select>
-    
 </body>
 </html>
